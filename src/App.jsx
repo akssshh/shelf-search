0bddb5f2ec9fd00f43ea50eb0d8e55be1961ef15
Akash Gogoi <gogoiakash09@gmail.com>
2024-02-10 19:38:33 +0530

    fix image size

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,11 @@ function App() {
 
   const fetchData = async () => {
     const response = await fetch(
-      "https://www.googleapis.com/books/v1/volumes?q=sudhamurty&key=AIzaSyBa4rUg7SG2Qr7QY-EfptJV1w7IvcylGK4"
+      "https://www.googleapis.com/books/v1/volumes?q=elon musk&key=AIzaSyBa4rUg7SG2Qr7QY-EfptJV1w7IvcylGK4"
     );
 
     const books = await response.json();
-    console.log(books.items[0].volumeInfo.imageLinks.smallThumbnail);
+    console.log(books.items[0].volumeInfo.imageLinks.thumbnail);
     setBooks(books.items);
   };
 
@@ -34,7 +34,7 @@ function App() {
                 <>
                   <div key={book.id} className="book">
                 <img
-                  src={book.volumeInfo.imageLinks?.thumbnail}
+                  src={book.volumeInfo.imageLinks.thumbnail}
                   className="rounded-md"
                   alt="book"
                   width={200}
